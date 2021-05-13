49f0c89fe39c4cfbee84c6e450b0c946501844d4
Dulatto <dulat.omarov@gmail.com>
2021-05-13 00:13:12 -0400

    build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -6,14 +6,18 @@ import BuildControl from './BuildControl/BuildControl';
 const controls = [
     { label: 'Salad', type: 'salad' },
     { label: 'Bacon', type: 'bacon' },
-    { label: 'Cheese', type: 'cheese' },
-    { label: 'Meat', type: 'meat' }
+    { label: 'Meat', type: 'meat' },
+    { label: 'Cheese', type: 'cheese' }
+
 ];
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label} />
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)} />
         ))}
     </div>
 
