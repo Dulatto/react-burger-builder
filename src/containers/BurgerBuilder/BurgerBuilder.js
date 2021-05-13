3f9cc0ba7eaa401d50d44d105003589e9ee664d4
Dulatto <dulat.omarov@gmail.com>
2021-05-12 23:59:20 -0400

    build controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,7 +3,12 @@ import Burger from '../../components/Burger/Burger';
 import Aux from '../../hoc/auxComponent';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
-
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 1.3,
+    meat: 0.7,
+    bacon: 0.4
+}
 
 class BurgerBuilder extends Component {
     // constructor(props){
@@ -16,9 +21,26 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
     }
 
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+    }
+
+    removeIngredientHandler = (type) => {
+
+    }
     render() {
         return (
             <Aux>
