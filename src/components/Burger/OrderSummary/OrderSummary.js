45ede1abc8b9021cce2cdb5a61608a83fedd152f
Dulatto <dulat.omarov@gmail.com>
2021-05-15 22:53:49 -0400

    modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -7,7 +7,7 @@ const orderSummary = (props) => {
 
     const ingredientSummary = Object.keys(props.ingredients)
         .map(igKey => {
-            return (<li>
+            return (<li key={igKey}>
                 <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
             </li>);
         });
@@ -18,8 +18,9 @@ const orderSummary = (props) => {
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
-
+                {ingredientSummary}
             </ul>
+            <p>Continue to Checkout?</p>
         </Aux>
     )
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,7 @@ import Burger from '../../components/Burger/Burger';
 import Aux from '../../hoc/auxComponent';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -79,7 +80,9 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
-                <Modal />
+                <Modal >
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal >
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
