6c3e2fa2ef5ae1471745ca24a2fa85b1e714c398
Dulatto <dulat.omarov@gmail.com>
2021-05-17 23:27:46 -0400

    toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import classes from './Toolbar.css';
 
-const toolbar = () => (
+const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>Menu</div>
         <div>LOGO</div>
