b4c8020be79dc885055cb07678031d9d25cf3ce4
Dulatto <dulat.omarov@gmail.com>
2021-05-17 23:33:01 -0400

    toolbar

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import burgerLogo from '../../../assets/images/burger-logo';
+import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css';
 
 const logo = (props) => (
