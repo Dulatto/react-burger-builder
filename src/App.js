6e1e49fb010bee76184295f6028aa4146f70fec7
Dulatto <dulat.omarov@gmail.com>
2021-05-25 23:59:04 -0400

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout/Layout';
+
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Layout from './containers/Layout/Layout';
 
 
 class App extends Component {
   render() {
     return (
       <div >
-        <Layout >
+        <Layout>
           <BurgerBuilder />
         </Layout>
       </div>
