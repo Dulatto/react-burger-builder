1b6a65e3546bef57934eb76e162fd00213099ba9
Dulatto <dulat.omarov@gmail.com>
2021-06-14 21:42:00 -0400

    checkout

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+
 
 const checkoutSummary = (props) => {
     return (
         <div>
             <h1>We hope it tastes well!</h1>
+            <div style={{ width: '300px', height: '300px', margin: 'auto' }}>
+                <Burger ingredients={props.ingredients} />
+            </div>
+            <Button btnType="Danger">Cancel</Button>
         </div>
     );
 }
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,19 +1,8 @@
 import React, { Component } from 'react'
 
 class Checkout extends Component {
-    constructor(props) {
-        super(props)
 
-        this.state = {
 
-        }
-    }
-
-    render() {
-        return (
-            
-        )
-    }
 }
 
 export default Checkout
