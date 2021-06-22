dfba28cd34aa0720c6be54ccf27424a2b02f0e65
Dulatto <dulat.omarov@gmail.com>
2021-06-22 00:00:09 -0400

    form

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,4 +4,22 @@ import React from 'react';
 
 const input = (props) => {
 
-}
+    let inputElement = null;
+
+    switch (props.inputType) {
+        case ('input'):
+            inputElement = <input />
+    }
+
+    return (
+        <div>
+            <label>{props.label}</label>
+
+        </div>
+    );
+
+
+};
+
+
+export default input;
